7e7fe980c1fb9f8a9f799fb09e2eb71632aaaacf
wangkuixing <wangkx0902@163.com>
2020-12-01 17:09:45 +0800

    add docker rm

--- jenkins/scripts/build.sh
+++ jenkins/scripts/build.sh
@@ -5,4 +5,13 @@ tag=`date +%Y%m%d%H%M`
 tag='jenkinsfiledemo:'$tag
 docker build -t $tag .
 echo $tag
+
+app_name='jenkinsfiledemo'
+docker stop ${app_name}
+echo '----stop container----'
+docker rm ${app_name}
+echo '----rm container----'
+docker rmi `docker images | grep none | awk '{print $3}'`
+echo '----rm none images----'
 docker run -p 8080:8080 --name jenkinsfiledemo -d $tag
+echo '----start container----'
